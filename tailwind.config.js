baae96d73f4dda06212bf467654e705a2b1766a7
Holy <oleg.chikolovec@gmail.com>
2024-03-25 18:24:41 +0300

    update colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,14 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-  colors: {
-    darkbrown: '#5b4f47',
-    daksand: '#c9b7a9',
-    sand: '#d6c4b5',
-    lightsand: '#e2d3bf',
-    brown: '#997c54',
-    rose: '#fec4d2'
-    },
+  //colors: {
+    //darkbrown: '#5b4f47',
+   // daksand: '#c9b7a9',
+   // sand: '#d6c4b5',
+   // lightsand: '#e2d3bf',
+   // brown: '#997c54',
+    //rose: '#fec4d2'
+   // },
 
     extend: {
       animation: {
