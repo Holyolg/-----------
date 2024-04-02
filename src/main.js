37bc4a646a89a7850627f05b051f0097cc7bd6a0
Holyolg <oleg.chikolovec@gmail.com>
2024-04-02 23:08:49 +0300

    add swipper

--- src/main.js
+++ src/main.js
@@ -186,4 +186,23 @@ function validTel () {
 	}
  });
 
+ const swiper = new Swiper('.swiper', {
+	// Optional parameters
+	direction: 'horizontal',
+	loop: true,
+	effect: 'cards',
+  
+	// If we need pagination
+	pagination: {
+	  el: '.swiper-pagination',
+	},
+  
+	// Navigation arrows
+	navigation: {
+	  nextEl: '.swiper-button-next',
+	  prevEl: '.swiper-button-prev',
+	},
+
+  });
+
 });
